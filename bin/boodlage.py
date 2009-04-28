cd06486ca608463da14ea425645b51096df0908f
James Smith <james@floppy.org.uk>
2009-04-28 22:16:24 +0100

    added first stab at controller script

---
+++ bin/boodlage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import simplejson, urllib, os, time
+
+while(1):
+  # Load frequency data
+  url = "http://caniturniton.com/api/json"  
+  result = simplejson.load(urllib.urlopen(url))
+  f = result['decision']['frequency']
+  print f
+  # scale up
+  diff = f - 50.0
+  diff = diff * 300
+  f = 200.0 + diff
+  # set pitch
+  os.system("boodle-event.py pitch {0}".format(f))
+  # Wait
+  time.sleep(15)
+  
