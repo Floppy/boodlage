fb8ab673b937016c54f55ddf27c1a1bd0ee7663c
James Smith <james@floppy.org.uk>
2009-04-28 17:48:58 +0100

    add a simple soundscape that responds to an event

---
+++ soundscapes/simple/main.py
@@ -0,0 +1,19 @@
+from boopak.package import *
+from boodle import agent
+
+water = bimport('org.boodler.old.water')
+
+class Simple(agent.Agent):
+
+  def init(self):
+    self.delay = 1
+  
+  def run(self):
+    if (self.firsttime):
+      self.listen('delay')
+    self.sched_note(water.droplet_plink)
+    self.resched(self.delay)
+    
+  def receive(self, event, delay):
+    self.delay = float(delay)
+
