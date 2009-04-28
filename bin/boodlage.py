b92a9be9f576fafd4cfe2c6c990749fb7c768489
James Smith <james@floppy.org.uk>
2009-04-28 23:11:55 +0100

    generalise code a lot more, and talk to boodler directly over a TCP connection

--- bin/boodlage.py
+++ bin/boodlage.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python
 
-import simplejson, urllib, os, time
+import urllib, time, re, telnetlib
+
+url = "http://www.nationalgrid.com/ngrealtime/realtime/systemdata.aspx"
+match = ".*Frequency: (.*?)Hz.*"
+in_min = 49.8
+in_max = 50.2
+interval = 15
+
+host = "localhost"
+event = "pitch"
+out_min = 140.0
+out_max = 260.0
+
+in_range = in_max - in_min
+out_range = out_max - out_min
+multiplier = out_range / in_range
 
 while(1):
   # Load frequency data
-  url = "http://caniturniton.com/api/json"  
-  result = simplejson.load(urllib.urlopen(url))
-  f = result['decision']['frequency']
-  print f
+  result = urllib.urlopen(url).read()
+  regexp = re.compile(match)
+  f = regexp.findall(result)[0]
   # scale up
-  diff = f - 50.0
-  diff = diff * 300
-  f = 200.0 + diff
+  x = float(f) - in_min
+  f = out_min + (x * multiplier)
   # set pitch
-  os.system("boodle-event.py pitch {0}".format(f))
+  tn = telnetlib.Telnet(host, 31863)
+  tn.write("{0} {1}\n".format(event, f))
   # Wait
-  time.sleep(60)
+  time.sleep(interval)
   
